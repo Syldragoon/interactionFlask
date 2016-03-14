90c3ca454af9f1b0bbe139da2d6dd35806ad3fe5
Sylvain <syldragoon@gmail.com>
2016-03-14 23:24:12 +0000

    Capture images from Raspberry Camera on click

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 #Import begin
+import time
 #Flask
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+#Pi camera
+import picamera
 #Import end
+
+#Init camera
+camera = picamera.PiCamera()
+time.sleep(2)
 
 #Create Flask app
 app = Flask(__name__)
 
-@app.route('/image')
+@app.route('/image/display', methods=['GET'])
 def displayImage():
+    #Render image layout
     return render_template('image.html')
+
+@app.route('/image/capture',  methods=['GET'])
+def captureImage():
+    #Capture image from camera
+    img_path = 'static/capture.jpg'
+    camera.capture(img_path)
+
+    #Return path for capture
+    return jsonify(result = img_path)
 
 if __name__ == '__main__':
     app.secret_key = 'password'
