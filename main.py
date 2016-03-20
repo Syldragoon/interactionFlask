f1abab6aea53b1bef0a8adc6d02765e390eb82b7
Sylvain <syldragoon@gmail.com>
2016-03-20 22:39:59 +0000

    Add choice for camera resolution and fix not closed camera issue

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 import picamera
 #Import end
 
-#Init camera
-camera = picamera.PiCamera()
-time.sleep(2)
+#Utility methods begin
+def isAsInt(iStr):
+    '''Check whether or not a string can be casted to an int'''
+    asInt = False
+    try:
+        int(iStr)
+        asInt = True
+    except ValueError:
+        pass
+    return asInt
+#Utility methods end
 
 #Create Flask app
 app = Flask(__name__)
@@ -21,8 +29,26 @@ def displayImage():
 @app.route('/image/capture',  methods=['GET'])
 def captureImage():
     #Capture image from camera
+    #Variables
     img_path = 'static/capture.jpg'
-    camera.capture(img_path)
+
+    #Default resolution
+    res_w = 640
+    res_h = 480
+
+    #Retrieve resolution if possible
+    resolution = request.args.get('resolution', '', type=str)
+    widthHeight = resolution.split('x')
+    if len(widthHeight) == 2 and isAsInt(widthHeight[0]) and isAsInt(widthHeight[1]):
+        res_w = int(widthHeight[0])
+        res_h = int(widthHeight[1])
+        print 'Resolution retrieved: %dx%d' % (res_w, res_h)
+
+    #Operate on camera
+    with picamera.PiCamera() as camera:
+        time.sleep(2)
+        camera.resolution = (res_w, res_h)
+        camera.capture(img_path)
 
     #Return path for capture
     return jsonify(result = img_path)
